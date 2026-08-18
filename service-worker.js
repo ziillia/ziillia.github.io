cecfc64b36b4fa5313042069e76b2d71e5b4c94e
ziillia <kei634579@gmail.com>
2026-08-19 01:29:22 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='drive-dj-finder-v1.4.4-relink';
+const CACHE_NAME='drive-dj-finder-v1.4.5-dictionary-first';
 const APP_SHELL=['./','./drive-dj-finder.html','./musicbrainz-ocr-dictionary.js','./manifest.webmanifest','./pwa-icon.svg','./pwa-icon-180.png','./pwa-icon-512.png'];
 
 self.addEventListener('install',event=>{
